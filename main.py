8962b4413bcc350031242e90431f031f62514896
mlussati <mlussati@hotmail.com>
2025-07-14 07:59:59 -0300

    Gera um usuário fictício via LLM e retorna como um objeto Python validado.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import os
 from helper import get_openai_api_key
+from openai import OpenAI
+from pydantic import BaseModel
+from typing import Optional
 
 KEY = get_openai_api_key()
 
-print(KEY)
+# How to Use Structured Outputs
+client = OpenAI(
+    api_key=KEY
+)
+
+# Define structure with Pydantic
+class User(BaseModel):
+    name: str
+    age: int
+    email: Optional[str] = None
+
+completion = client.beta.chat.completions.parse(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Make up a user."},
+    ],
+    response_format=User,
+)
+user = completion.choices[0].message.parsed
+print(user)
